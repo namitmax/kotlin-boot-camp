e6bc8a1763d8e5e49899c4aaf90632362ed3f365
namitmax <honun@mail.ru>
2018-10-08 18:43:17 +0300

    fix

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,7 +6,15 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-fun max(values: List<Int>): Int = TODO()
+fun max(values: List<Int>): Int {
+    if (values.isNotEmpty()) {
+        var mx: Int = values.first()
+        for (e in values) {
+            if (e > mx) mx = e
+        }
+        return mx
+    } else return 0
+}
 
 /**
  * Returns the sum of all `int` values.
@@ -14,4 +22,12 @@ fun max(values: List<Int>): Int = TODO()
  * @param values an argument. Assume values.length > 0.
  * @return the sum of all values.
  */
-fun sum(values: List<Int>): Long = TODO()
+fun sum(values: List<Int>): Long {
+    if (values.isNotEmpty()) {
+        var sm: Long = 0
+        for (e in values) {
+            sm += e
+        }
+        return sm
+    } else return 0
+}
